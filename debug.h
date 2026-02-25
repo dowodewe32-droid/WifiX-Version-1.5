23abacf38100f5b6f07018a1a4d75cbe65af9e14
dowodewe32-droid <dowodewe32@gmail.com>
2026-02-25 21:41:34 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,17 +1,25 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#include "config.h"      // HARUS DI LUAR
-#include <Arduino.h>     // HARUS DI LUAR
+// 1. PINDAHKAN INCLUDE KE LUAR DARI EXTERN "C"
+#include "config.h"
+#include <Arduino.h>
 
 #ifdef __cplusplus
-extern "C" {            // Mulai blok C di SINI
+extern "C" {
+#endif
+    // Hanya fungsi C murni yang boleh di sini (jika ada)
+#ifdef __cplusplus
+}
 #endif
 
-// ... sisa kode di dalam sini aman ...
-
-#ifdef __cplusplus
-}                       // Tutup blok C
+// 2. DEFINE MACRO DEBUG DI LUAR EXTERN "C" KARENA PAKAI C++ (Serial)
+#ifdef ENABLE_DEBUG
+    #define debugF(...) DEBUG_PORT.print(__VA_ARGS__)
+    #define debuglnF(...) DEBUG_PORT.println(__VA_ARGS__)
+#else
+    #define debugF(...)
+    #define debuglnF(...)
 #endif
 
 #endif
