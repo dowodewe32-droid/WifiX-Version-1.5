63f12cf99bbc66677c59a05b207a44cf83a7ac12
dowodewe32-droid <dowodewe32@gmail.com>
2026-02-25 23:51:36 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,12 +1,11 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-// 1. WAJIB DI LUAR: Semua include harus di atas, sebelum logika apa pun
-#include "config.h"
 #include <Arduino.h>
 
-// 2. JANGAN pakai extern "C" karena Serial adalah fitur C++
-// Ini yang bikin error "template with C linkage" di log lo.
+// JANGAN pakai extern "C" karena akan menyebabkan error "template with C linkage"
+#define ENABLE_DEBUG
+#define DEBUG_PORT Serial
 
 #ifdef ENABLE_DEBUG
   #define debug(...) DEBUG_PORT.print(__VA_ARGS__)
