f37aa3b6fdc53247471c52ce6b0c779def5d790e
dowodewe32-droid <dowodewe32@gmail.com>
2026-02-25 20:29:13 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,41 +1,27 @@
-/* This software is licensed under the MIT License: https://github.com/spacehuhntech/esp8266_deauther */
+#ifndef DEBUG_H
+#define DEBUG_H
 
-#pragma once
+#include <Arduino.h>
 
-#include "config.h"
+// JANGAN naruh #include di bawah extern "C"
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#include <climits>   // LONG_MAX
-#include <Arduino.h> // Serial
+void debug_setup();
 
-#ifdef ENABLE_DEBUG
+#ifdef __cplusplus
+}
+#endif
 
-#define debug_init()\
-    DEBUG_PORT.begin(DEBUG_BAUD);\
-    DEBUG_PORT.setTimeout(LONG_MAX);\
-    DEBUG_PORT.println();
-
-#define debug(...) DEBUG_PORT.print(__VA_ARGS__)
-#define debugln(...) DEBUG_PORT.println(__VA_ARGS__)
-#define debugf(...) DEBUG_PORT.printf(__VA_ARGS__)
-#define debugF(...) DEBUG_PORT.print(F(__VA_ARGS__))
-#define debuglnF(...) DEBUG_PORT.println(F(__VA_ARGS__))
-
-#define debug_available() DEBUG_PORT.available()
-#define debug_read() DEBUG_PORT.read()
-#define debug_peek() DEBUG_PORT.peek()
-
-#else /* ifdef ENABLE_DEBUG */
-
-#define debug_init() 0
-
-#define debug(...) 0
-#define debugln(...) 0
-#define debugf(...) 0
-#define debugF(...) 0
-#define debuglnF(...) 0
-
+#define debug_init() 
+#define debug(...) 
+#define debugln(...) 
+#define debugf(...) 
+#define debugF(...) 
+#define debuglnF(...) 
 #define debug_available() 0
 #define debug_read() 0
 #define debug_peek() 0
 
-#endif /* ifdef ENABLE_DEBUG */
+#endif
