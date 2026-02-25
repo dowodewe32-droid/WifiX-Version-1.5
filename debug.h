b3cb74320b12cd1e4c436e8a1bbef3e8c30a4c90
dowodewe32-droid <dowodewe32@gmail.com>
2026-02-25 21:37:22 +0800

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,27 +1,17 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#include <Arduino.h>
-
-// JANGAN naruh #include di bawah extern "C"
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void debug_setup();
+#include "config.h"      // HARUS DI LUAR
+#include <Arduino.h>     // HARUS DI LUAR
 
 #ifdef __cplusplus
-}
+extern "C" {            // Mulai blok C di SINI
 #endif
 
-#define debug_init() 
-#define debug(...) 
-#define debugln(...) 
-#define debugf(...) 
-#define debugF(...) 
-#define debuglnF(...) 
-#define debug_available() 0
-#define debug_read() 0
-#define debug_peek() 0
+// ... sisa kode di dalam sini aman ...
+
+#ifdef __cplusplus
+}                       // Tutup blok C
+#endif
 
 #endif
