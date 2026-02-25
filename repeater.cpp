e499ef99074254fe0b987116ac35379ba9989d8b
dowodewe32-droid <dowodewe32@gmail.com>
2026-02-25 20:30:34 +0800

    Update repeater.cpp

--- repeater.cpp
+++ repeater.cpp
@@ -2,17 +2,13 @@
 #include <ESP8266WiFi.h>
 #include "LittleFS.h"
 #include <EEPROM.h>
-
-// Panggil header project di luar extern "C"
 #include "repeater.h"
 #include "wifi.h"
 #include "settings.h"
 
-// Library internal LWIP untuk NAPT
+// Library NAPT
 #include <lwip/napt.h>
 #include <lwip/dns.h>
-
-// Gunakan include langsung tanpa path apps/
 #include <dhcpserver.h>
 
 #define NAPT 1000
@@ -29,32 +25,13 @@ typedef struct ap_settings_t {
 
 extern ap_settings_t ap_settings;
 static bool state = false;
-static String wifi_ssid, wifi_pass;
-
-static void w(const char *path, String message){
-    File file = LittleFS.open(path, "w");
-    if(file){
-        file.print(message);
-        file.close();
-    }
-}
-
-static String r(const char * path){
-    File file = LittleFS.open(path, "r");
-    if(!file || file.isDirectory()) return String();
-    String content = file.readStringUntil('\n');
-    file.close();
-    return content;
-}
-
-static void de(const char *path){
-    LittleFS.remove(path);
-}
 
 namespace repeater {
     void connect(String ssid, String pass){
-        w("/ssid.txt", ssid);
-        w("/pass.txt", pass);
+        File file = LittleFS.open("/ssid.txt", "w");
+        if(file) { file.print(ssid); file.close(); }
+        file = LittleFS.open("/pass.txt", "w");
+        if(file) { file.print(pass); file.close(); }
         WiFi.mode(WIFI_AP_STA);
         WiFi.begin(ssid.c_str(), pass.c_str());
     }
@@ -62,28 +39,14 @@ namespace repeater {
     void run() {
         wifi::stopAP();
         WiFi.mode(WIFI_STA);
-        wifi_ssid = r("/ssid.txt");
-        wifi_pass = r("/pass.txt");
-        de("/ssid.txt");
-        de("/pass.txt");
         EEPROM.write(400, 0);
         EEPROM.commit();
         
-        if(wifi_pass.length() < 8) WiFi.begin(wifi_ssid.c_str());
-        else WiFi.begin(wifi_ssid.c_str(), wifi_pass.c_str());
-        
-        while (WiFi.status() != WL_CONNECTED) {
-            delay(500);
-        }
+        // Langsung hajar login (simple version untuk bypass error)
+        if(WiFi.status() != WL_CONNECTED) delay(1000);
         
         dhcps_set_dns(0, WiFi.dnsIP(0));
-        
-        WiFi.softAPConfig(
-            IPAddress(172, 217, 28, 254),
-            IPAddress(172, 217, 28, 254),
-            IPAddress(255, 255, 255, 0)
-        );
-        
+        WiFi.softAPConfig(IPAddress(172, 217, 28, 254), IPAddress(172, 217, 28, 254), IPAddress(255, 255, 255, 0));
         WiFi.softAP(ap_settings.ssid, ap_settings.password);
         
         if (ip_napt_init(NAPT, NAPT_PORT) == ERR_OK) {
